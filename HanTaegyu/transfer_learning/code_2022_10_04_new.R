a0fb6273236d77612df67c7b62974829d337f94e
eui20n <eui20n@naver.com>
2022-10-05 15:00:49 +0900

    Combine

--- HanTaegyu/transfer_learning/code_2022_10_04_new.R
+++ HanTaegyu/transfer_learning/code_2022_10_04_new.R
@@ -23,14 +23,14 @@ library(stringr)
 # --------------------------------------------- #
 # 질문
 # 1. 교수님의 따릉이는 날짜데이터를 뺏는데, 현재 데이터도 날짜데이터를 빼도 될지?
-#
+# 2. csv로 빼는 부분에서 왜 시점 23과 24는 같은지? 이것이 의미하는 것이 무엇인지?
 #
 
 # --------------------------------------------- # 
 # 데이터 불러오기
 
 # 프로젝트 경로
-DEFAULT_PATH <- "C:\\Users\\student\\Desktop\\solar\\Solar_radiation_prediction_model\\HanTaegyu\\transfer_learning"
+DEFAULT_PATH <- "D:/githubManagement/Solar_radiation_prediction_model/HanTaegyu/transfer_learning"
 setwd(DEFAULT_PATH)
 getwd()
 # [1] "D:/githubManagement/Solar_radiation_prediction_model/HanTaegyu/transfer_learning"
@@ -185,21 +185,21 @@ if (pc_number == 1) {
 }
 
 # 11시점을 예측하기 위해서 11 만큼만 봐줌
-# loopnum
+loopnum
 for(i in run_seq){
   i %>% print()
   model.ranger <- ranger(
-    Solar~., data = rbind(train.data, rbind(train.data, test.data[i:(i + 10),])), 
-    num.tree = 128, 
-    mtry = 2, 
+    Solar~., data = rbind(train.data, rbind(train.data, test.data[i:(i + 10),])),
+    num.tree = 128,
+    mtry = 2,
     importance = "impurity"
   )
-  
+
   # method 1
   # for(idx in 1:data.pred %>% length()){
   #   get(data.pred[idx])[i] <- predict(model.ranger, data = test.data[i+idx-1,])
   # }
-  
+
   # method 2
   data.pred01[i] <- predict(model.ranger, data = test.data[i + 11,]) %>% as.numeric()
   data.pred02[i] <- predict(model.ranger, data = test.data[i+ 12,]) %>% as.numeric()
@@ -216,17 +216,17 @@ for(i in run_seq){
 
 # 결과 저장
 
-save(data.pred01, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "01", ".Rdata"))
-save(data.pred02, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "02", ".Rdata"))
-save(data.pred03, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "03", ".Rdata"))
-save(data.pred04, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "04", ".Rdata"))
-save(data.pred05, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "05", ".Rdata"))
-save(data.pred06, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "06", ".Rdata"))
-save(data.pred07, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "07", ".Rdata"))
-save(data.pred08, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "08", ".Rdata"))
-save(data.pred09, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "09", ".Rdata"))
-save(data.pred10, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "10", ".Rdata"))
-save(data.pred11, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "11", ".Rdata"))
+# save(data.pred01, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "01", ".Rdata"))
+# save(data.pred02, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "02", ".Rdata"))
+# save(data.pred03, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "03", ".Rdata"))
+# save(data.pred04, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "04", ".Rdata"))
+# save(data.pred05, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "05", ".Rdata"))
+# save(data.pred06, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "06", ".Rdata"))
+# save(data.pred07, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "07", ".Rdata"))
+# save(data.pred08, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "08", ".Rdata"))
+# save(data.pred09, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "09", ".Rdata"))
+# save(data.pred10, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "10", ".Rdata"))
+# save(data.pred11, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "11", ".Rdata"))
 
 
 # load(file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "01", ".Rdata"))
@@ -240,3 +240,76 @@ save(data.pred11, file = paste0(".\\predict", "\\", "PC", pc_number %>% as.chara
 # load(file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "09", ".Rdata"))
 # load(file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "10", ".Rdata"))
 # load(file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "11", ".Rdata"))
+
+# 각각의 번호 별로 나눠서 저장하기
+result01 <- data.frame()
+result02 <- data.frame()
+result03 <- data.frame()
+result04 <- data.frame()
+result05 <- data.frame()
+result06 <- data.frame()
+result07 <- data.frame()
+result08 <- data.frame()
+result09 <- data.frame()
+result10 <- data.frame()
+result11 <- data.frame()
+
+result.num <- c(
+  'result01', 'result02', 'result03', 'result04',
+  'result05', 'result06', 'result07', 'result08',
+  'result09', 'result10', 'result11'
+)
+
+for(i in 1:6){
+  pc_number <- i
+  
+  load(file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "01", ".Rdata"))
+  load(file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "02", ".Rdata"))
+  load(file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "03", ".Rdata"))
+  load(file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "04", ".Rdata"))
+  load(file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "05", ".Rdata"))
+  load(file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "06", ".Rdata"))
+  load(file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "07", ".Rdata"))
+  load(file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "08", ".Rdata"))
+  load(file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "09", ".Rdata"))
+  load(file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "10", ".Rdata"))
+  load(file = paste0(".\\predict", "\\", "PC", pc_number %>% as.character(), "_predict", "11", ".Rdata"))
+  
+  if (pc_number == 1) {
+    run_seq <- 1:1336  
+  } else if (pc_number == 2) {
+    run_seq <- 1337:2672  
+  } else if (pc_number == 3) {
+    run_seq <- 2673:4008
+  } else if (pc_number == 4) {
+    run_seq <- 4009:5344  
+  } else if (pc_number == 5) {
+    run_seq <- 5345:6680
+  } else if (pc_number == 6) {
+    run_seq <- 6681:8016
+  }
+  
+  # method1
+  # result01 <- rbind(result01, as.data.frame(data.pred01[run_seq]))
+  # result02 <- rbind(result02, as.data.frame(data.pred02[run_seq]))
+  # result03 <- rbind(result03, as.data.frame(data.pred03[run_seq]))
+  # result04 <- rbind(result04, as.data.frame(data.pred04[run_seq]))
+  # result05 <- rbind(result05, as.data.frame(data.pred05[run_seq]))
+  # result06 <- rbind(result06, as.data.frame(data.pred06[run_seq]))
+  # result07 <- rbind(result07, as.data.frame(data.pred07[run_seq]))
+  # result08 <- rbind(result08, as.data.frame(data.pred08[run_seq]))
+  # result09 <- rbind(result09, as.data.frame(data.pred09[run_seq]))
+  # result10 <- rbind(result10, as.data.frame(data.pred10[run_seq]))
+  # result11 <- rbind(result11, as.data.frame(data.pred11[run_seq]))
+  
+  
+  # method2 -> 반복문을 활용해서 위의 반복을 줄임
+  for(idx in 1:11){
+    assign(
+      result.num[idx],
+      rbind(get(result.num[idx]), as.data.frame(get(data.pred[idx])[run_seq]))
+    )
+  }
+  
+}
+
